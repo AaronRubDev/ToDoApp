6bcbe1e753306b3b813e9db4803087705887103c
Aaron <aaronrubidev@gmail.com>
2024-09-23 16:03:38 -0600

    layout design: icons added, text added, floating action button added

--- app/src/main/java/com/aaronrubidev/to_doapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/aaronrubidev/to_doapp/fragments/list/ListFragment.kt
@@ -5,7 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.aaronrubidev.to_doapp.R
+import com.aaronrubidev.to_doapp.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+
+    private lateinit var binding: FragmentListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentListBinding.inflate(inflater, container, false)
+
+        binding.floatingActionBottom.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return binding.root
+    }
 
 }
