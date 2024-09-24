88f308f5487b4ad35cd1bde22baa4181a739a8fd
Aaron <aaronrubidev@gmail.com>
2024-09-23 18:51:38 -0600

    Toolbar added

--- app/src/main/java/com/aaronrubidev/to_doapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/aaronrubidev/to_doapp/fragments/add/AddFragment.kt
@@ -5,7 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.aaronrubidev.to_doapp.R
+import com.aaronrubidev.to_doapp.databinding.FragmentAddBinding
+import com.aaronrubidev.to_doapp.databinding.FragmentListBinding
 
 class AddFragment : Fragment() {
+    private lateinit var binding: FragmentAddBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAddBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
 }
