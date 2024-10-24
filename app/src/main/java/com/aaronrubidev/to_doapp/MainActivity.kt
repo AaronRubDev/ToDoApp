a5cc8c36d20497390b27abe9844ad786b2439c85
Aaron <aaronrubidev@gmail.com>
2024-10-24 14:53:01 -0600

    Hotfix: Modified size, conflicts solution

--- app/src/main/java/com/aaronrubidev/to_doapp/MainActivity.kt
+++ app/src/main/java/com/aaronrubidev/to_doapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.aaronrubidev.to_doapp
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
